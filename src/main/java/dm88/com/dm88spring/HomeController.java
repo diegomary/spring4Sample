249d0256354bc6985b48f97d115390c77b8eb278
Diego Burlando <diego@dmm888.com>
2018-01-19 17:19:36 +0100

    Added different routes to read data in mongodb

--- src/main/java/dm88/com/dm88spring/HomeController.java
+++ src/main/java/dm88/com/dm88spring/HomeController.java
@@ -1,9 +1,5 @@
 package dm88.com.dm88spring;
-
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import models.Flower;
+import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
@@ -11,23 +7,20 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.management.Query;
+import models.Flower;
 import models.Student;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.query.BasicQuery;
-import org.springframework.data.mongodb.core.query.Criteria;
 
 @Controller
 @RequestMapping("/")
@@ -71,32 +64,105 @@ public class HomeController {
         {
             return new Student(id,name,lastName, email);
         }
-        
-        
-        
-        
        
-        @RequestMapping(value = "/flower")  
+        @RequestMapping(value = "/allflowers")  
         public @ResponseBody  
-        org.bson.Document getFlower()
-        {                  
-            MongoCredential credential = MongoCredential.createCredential("booksadmin", "diegomary88", "diegomary".toCharArray());
-            //MongoClient mongoClient = new MongoClient(new ServerAddress("ds061371.mlab.com",61371), Arrays.asList(credential));
+        List<org.bson.Document> getFlowers()
+        {        
             MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://booksadmin:diegomary@ds061371.mongolab.com:61371/diegomary88"));
             MongoDatabase db = mongoClient.getDatabase("diegomary88");         
-            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");          
-            long f =  mc.count();           
-            FindIterable<org.bson.Document> Flowers = mc.find();
-            org.bson.Document d = Flowers.first();   
-            return d;        
-        
+            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");        
+            long f =  mc.count();                        
+            List<org.bson.Document> all;
+            all = mc.find().into(new ArrayList<>());
+            return all;            
         }
         
+        @RequestMapping(value = "/allflowersnames")  
+        public @ResponseBody  
+        List<String> getFlowers1()
+        {                 
+            MongoCredential credential = MongoCredential.createCredential("booksadmin", "diegomary88", "diegomary".toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress("ds061371.mlab.com",61371), Arrays.asList(credential));           
+            MongoDatabase db = mongoClient.getDatabase("diegomary88");         
+            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");                   
+            FindIterable<org.bson.Document> it = mc.find();         
+            List<String> allFlowers = new ArrayList<>();
+            for (org.bson.Document document : it) {            
+                boolean add = allFlowers.add(document.getString("Name"));            
+            }           
+            return  allFlowers;
+            
+        }       
+        
+        
+        @RequestMapping(value = "/allflowersobjgson")  
+        public @ResponseBody  
+        String getFlowers2()
+        {                 
+            MongoCredential credential = MongoCredential.createCredential("booksadmin", "diegomary88", "diegomary".toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress("ds061371.mlab.com",61371), Arrays.asList(credential));           
+            MongoDatabase db = mongoClient.getDatabase("diegomary88");         
+            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");                   
+            FindIterable<org.bson.Document> it = mc.find();         
+            ArrayList<Flower> allFlowers = new ArrayList<>();
+            for (org.bson.Document document : it) {
+                allFlowers.add(new Flower(document.getInteger("Id"),
+                document.getString("Name"),
+                document.getString("Description"),
+                document.getString("ImagePath"),document.getString("FlowerColor"))
+                );            
+            }           
+            Gson gson = new Gson();
+            String json = gson.toJson(allFlowers);
+            return   json;            
+        } 
+        
+        @RequestMapping(value = "/allflowersobjjackson")  
+        public @ResponseBody  
+        String getFlowers3() throws IOException
+        {                 
+            MongoCredential credential = MongoCredential.createCredential("booksadmin", "diegomary88", "diegomary".toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress("ds061371.mlab.com",61371), Arrays.asList(credential));           
+            MongoDatabase db = mongoClient.getDatabase("diegomary88");         
+            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");                   
+            FindIterable<org.bson.Document> it = mc.find();         
+            ArrayList<Flower> allFlowers = new ArrayList<>();
+            for (org.bson.Document document : it) {
+                allFlowers.add(new Flower(document.getInteger("Id"),
+                document.getString("Name"),
+                document.getString("Description"),
+                document.getString("ImagePath"),document.getString("FlowerColor"))
+                );            
+            }           
+             ObjectMapper mapper = new ObjectMapper();
+             //mapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
+             return mapper.writeValueAsString(allFlowers);
+                    
+        } 
+        
+        @RequestMapping(value = "/allflowersautomatic")  
+        public @ResponseBody  
+        ArrayList<Flower> getFlowers4() throws IOException
+        {                 
+            MongoCredential credential = MongoCredential.createCredential("booksadmin", "diegomary88", "diegomary".toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress("ds061371.mlab.com",61371), Arrays.asList(credential));           
+            MongoDatabase db = mongoClient.getDatabase("diegomary88");         
+            MongoCollection<org.bson.Document> mc = db.getCollection("BachFlowers");                   
+            FindIterable<org.bson.Document> it = mc.find();         
+            ArrayList<Flower> allFlowers = new ArrayList<>();
+            for (org.bson.Document document : it) {
+                allFlowers.add(new Flower(document.getInteger("Id"),
+                document.getString("Name"),
+                document.getString("Description"),
+                document.getString("ImagePath"),document.getString("FlowerColor"))
+                );            
+            }           
+             
+             return allFlowers;
+                    
+        }      
         
         
         
-        
-        
-        
-
 }
--- src/main/java/models/Flower.java
+++ src/main/java/models/Flower.java
@@ -1,17 +1,15 @@
 package models;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
+//import org.springframework.data.mongodb.core.mapping.Document;
  
-@Document(collection="BachFlowers")
+//@Document(collection="BachFlowers")
 public class Flower {
-    @Id
-    int Id;
-    String Name;
-    String Description;
-    String ImagePath;
-    String FlowerColor;
+   
+  private   int Id;
+  private  String Name;
+  private  String Description;
+  private  String ImagePath;
+  private  String FlowerColor;
   
     public Flower(int id, String name, String description,String imagePath,String flowerColor){
         this.Id = id;
@@ -19,6 +17,46 @@ public class Flower {
         this.Description = description;
         this.ImagePath = imagePath;
         this.FlowerColor = flowerColor;
-    } 
+    }
+    
+    public int getId() {
+        return this.Id;
+    }
+    
+    public void setId(int num) {
+        this.Id = num;
+    }
+    
+    public String getName() {
+        return this.Name;
+    }
+    
+    public void setName(String name) {
+        this.Name = name;
+    }
+    
+    public String getDescription() {
+        return this.Description;
+    }
+    
+    public void setDescription(String description) {
+        this.Description = description;
+    }
+    
+    public String getImagePath() {
+        return this.ImagePath;
+    }
+    
+    public void setImagePath(String imagePath) {
+        this.ImagePath = imagePath;
+    }
+    
+    public String getFlowerColor() {
+        return this.FlowerColor;
+    }
+    
+    public void setFlowerColor(String flowerColor) {
+        this.FlowerColor = flowerColor;
+    }
    
 } 
